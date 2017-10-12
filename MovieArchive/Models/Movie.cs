3b2071617ce666b1cab829a158bc1cdde6fd1787
christopherie <christophergovenderkubiec@gmail.com>
2017-10-12 11:24:38 +0100

    Movie Model and Movies Controller

---
+++ MovieArchive/Models/Movie.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Data.Entity;
+
+namespace MovieArchive.Models
+{
+    public class Movie
+    {
+        public int ID { get; set; }
+
+        [StringLength(60, MinimumLength = 3)]
+        public string Title { get; set; }
+
+        [Display(Name = "Release Date")]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        public DateTime ReleaseDate { get; set; }
+
+        [RegularExpression(@"^[A-Z]+[a-zA-Z''-'\s]*$")]
+        [Required]
+        [StringLength(30)]
+        public string Genre { get; set; }
+
+        [RegularExpression(@"^[A-Z]+[a-zA-Z''-'\s]*$")]
+        [StringLength(5)]
+        public string Rating { get; set; }
+
+        [DataType(DataType.MultilineText)]
+        public string Review { get; set; }
+
+        [DataType(DataType.ImageUrl)]
+        [Display(Name = "Image Url")]
+        public string ImageUrl { get; set; }
+    }
+
+    /*
+     * The MovieDBContext class represents the Entity Framework movie database context, 
+     * which handles fetching, storing, and updating Movie class instances in a database. 
+     * The MovieDBContext derives from the DbContext base class provided by the Entity Framework.
+     */
+    public class MovieDBContext : DbContext
+    {
+        public DbSet<Movie> Movies { get; set; }
+    }
+}
